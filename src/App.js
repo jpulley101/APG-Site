1c05209927417a0da3ac31e64c5a76950c8c9708
jpulley101 <jarrettpulley@gmail.com>
2017-11-19 09:34:39 -0500

    comp folder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as Icon from 'react-feather';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+       <Icon.AlertCircle color="red" size={98} />
       </div>
     );
   }
